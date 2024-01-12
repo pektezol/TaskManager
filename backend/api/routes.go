e7e460ac9189689d92771df961ea05fe607d0f98
Arda Serdar Pektezol <1669855+pektezol@users.noreply.github.com>
2024-01-12 22:05:55 +0300

    feat: specify routes

--- backend/api/routes.go
+++ backend/api/routes.go
@@ -10,7 +10,33 @@ func InitRoutes(router *gin.Engine) {
 	api := router.Group("/api")
 	{
 		api.GET("/", controllers.Home)
+		// users
+		api.GET("/users") // user list
 		api.POST("/login", controllers.Login)
 		api.POST("/register", controllers.Register)
+		// projects
+		api.GET("/projects")                                    // project listd
+		api.GET("/projects/:projectid")                         // project get
+		api.POST("/projects")                                   // project create
+		api.DELETE("/projects/:projectid")                      // project delete
+		api.POST("/projects/:projectid/collaborator")           // project add collaborator
+		api.DELETE("/projects/:projectid/collaborator/:userid") // project remove collaborator
+		// tasks
+		api.GET("/projects/:projectid/tasks")                             // task list
+		api.GET("/projects/:projectid/tasks/:taskid")                     // task get
+		api.POST("/projects/:projectid/tasks")                            // task create
+		api.PUT("/projects/:projectid/tasks/:taskid")                     // task update
+		api.DELETE("/projects/:projectid/tasks/:taskid")                  // task delete
+		api.POST("/projects/:projectid/tasks/:taskid/comment")            // task add comment
+		api.POST("/projects/:projectid/tasks/:taskid/assignee")           // task add assignee
+		api.DELETE("/projects/:projectid/tasks/:taskid/assignee/:userid") // task remove assignee
+		// priority
+		api.GET("/priority") // priority list
+		// status
+		api.GET("/status") // status list
+		// notifications
+		api.GET("/notifications")                    // notification list
+		api.PUT("/notifications/:notificationid")    // notification update
+		api.DELETE("/notifications/:notificationid") // notification delete
 	}
 }
